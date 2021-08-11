8c25aed6e350bdb04c47158c9b4440a9be8ca304
James Vidler <james@agilitycms.com>
2021-08-10 23:08:13 -0400

    added basic block editor custom field

---
+++ block-editor/src/App.js
@@ -0,0 +1,35 @@
+
+import React, { useState, useEffect, useRef } from "react"
+import {
+	BrowserRouter as Router,
+	Switch,
+	Route,
+	Link
+} from "react-router-dom";
+import BlockEditor from "./BlockEditor";
+
+
+function App() {
+
+	return (
+	<BlockEditor />
+	// <Router>
+	// 	<div>
+	// 		<Switch>
+	// 			<Route path="/block-editor">
+	// 				<div>BLOCK</div>
+	// 				{/* <BlockEditor /> */}
+	// 			</Route>
+	// 			<Route path="/cloudinary">
+	// 				<CloudinaryMedia />
+	// 			</Route>
+	// 			<Route path="/gutenberg">
+	// 				<Gutenberg />
+	// 			</Route>
+	// 		</Switch>
+	// 	</div>
+	// </Router>
+	)
+}
+
+export default App;
