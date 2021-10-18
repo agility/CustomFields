ec2e044a9b4d1ea7ad298edd486970b51da42cdb
jamesvidler <james.vidler@outlook.com>
2021-10-18 12:00:53 -0400

    Added tag/value custom field

---
+++ tag-value-field/react-field/public/register-tag-value-field.js
@@ -0,0 +1,107 @@
+var TagValueCustomField = function() {
+
+    var self = this;
+    self.Label = "Tag Value Custom Field";
+    self.ReferenceName = "TagValueCustomField";
+    self.Render = function (options) {
+        /// <summary>Function called whenever the form container this Custom Field Type is rendered or refreshed.</summary>
+        /// <param name="options" type="Object">
+        ///     <field name="$elem" type="jQueryElem">The .field-row jQuery Dom Element.</field>
+        ///     <field name="contentItem" type="ContentItem Object">The entire Content Item object including Values and their KO Observable properties of all other fields on the form.</field>
+        ///     <field name="fieldBinding" type="KO Observable">The value binding of thie Custom Field Type. Get and set this field's value using this property.</field>
+        ///     <field name="fieldSetting" type="Object">Object representing the field's settings such as 'Hidden', 'Label', and 'Description'</field>
+        ///     <field name="readonly" type="boolean">Represents if this field should be readonly or not.</field>
+        /// </param>
+        setupIframe({
+            fieldLabel: self.Label,
+            fieldReferenceName: self.ReferenceName,
+            fieldOptions: options,
+            iFrameUrl: 'http://localhost:3000/',
+            iFrameWidth: '100%',
+            iFrameHeight: 'auto',
+            iFrameClassName: 'tag-value-custom-field'
+        })
+    }
+}
+
+ContentManager.Global.CustomInputFormFields.push(new TagValueCustomField());
+
+var setupIframe = function(params) {
+
+    var $pnl = $("." + params.iFrameClassName, params.fieldOptions.$elem);
+
+    if ($pnl.size() > 0) return; //already rendered...
+
+    var iFrameOrigin = function() {
+        var pathArray = params.iFrameUrl.split('/');
+        var protocol = pathArray[0];
+        var host = pathArray[2];
+        var origin = protocol + '//' + host;
+        return origin
+    }();
+
+    var config = ContentManager.ViewModels.Navigation.globalConfig();
+    var languageCode = ContentManager.ViewModels.Navigation.currentLanguageCode();
+    var fieldName = params.fieldOptions.fieldSetting.FieldName;
+    var fieldID = params.fieldOptions.fieldSetting.FieldID;
+
+    //set up the child iframe to render the field
+    var iframe = document.createElement('iframe');
+    iframe.className = params.iFrameClassName;
+    iframe.width = params.iFrameWidth;
+    iframe.height = params.iFrameHeight;
+    iframe.src = params.iFrameUrl + '?fieldName=' + fieldName + '&fieldID=' + fieldID;
+    iframe.onload = function() {}
+
+    //render the iframe
+    params.fieldOptions.$elem.html(iframe);
+
+    //listen for all iframe messages
+    window.addEventListener("message", function (e) {
+        
+        //only process messages from the child iframe
+        if(e.origin !== iFrameOrigin) return;
+        
+        switch (e.data.type) {
+            case 'fieldReady_for_' + fieldName + '_' + fieldID:
+                console.log(fieldName + '['+ params.fieldOptions.fieldSetting.Settings.CustomFieldType + '] (from CMS) => Sending auth and fieldValue message');
+                //send a message to the child iframe with the details of this field
+                iframe.contentWindow.postMessage({
+                    message: {
+                        auth: {
+                            guid: config.Guid,
+                            websiteName: config.WebsiteName,
+                            securityKey: config.SecurityKey,
+                            languageCode: languageCode,
+                            location: 'USA', //or 'CANADA',
+                            apiKey: 'defaultpreview.7dc1052104d1f593efd8f7934e913f70e5f7615a6e80970b5f18f4ebe6a0810c',
+                        },
+                        fieldValue: ko.unwrap(params.fieldOptions.fieldBinding),
+                        fieldLabel: params.fieldOptions.fieldSetting.Label,
+                        fieldName: fieldName,
+                        fieldID: fieldID,
+                        fieldReferenceName: params.fieldOptions.fieldSetting.Settings.CustomFieldType,
+                        //fieldOptions: params.fieldOptions,
+                        origin: window.location.href,
+                        tagsContentReferenceName: 'Tags',
+
+                    },
+                    type: 'setInitialProps_for_' + fieldName + '_' + fieldID
+                }, "*")
+
+                break
+            case 'setNewValue_for_' + fieldName + '_' + fieldID:
+                params.fieldOptions.fieldBinding(e.data.message);
+                break;
+            case 'setHeight_for_' + fieldName + '_' + fieldID:
+                iframe.height = e.data.message + "px"
+                break;
+
+            default:
+                //do nothing...
+                break;
+        }
+
+    }, false);
+    
+}
---
+++ tag-value-field/react-field/src/App.js
@@ -0,0 +1,138 @@
+import { useEffect, useState, useRef } from 'react';
+import './App.css';
+import { initializeField, updateFieldValue } from './agility-utils';
+import agility from '@agility/content-fetch';
+
+function App() {
+  const [tags, setTags] = useState([]);
+  const [auth, setAuth] = useState({});
+  const [value, setValue] = useState("");
+  const [fieldName, setFieldName] = useState("");
+  const [fieldID, setFieldID] = useState("");
+  const [tagOptions, setTagOptions] = useState([]);
+  const containerRef = useRef();
+
+
+  useEffect(() => {
+    initializeField({
+      containerRef,
+      //when field is ready, get the params (i.e. value and auth) from the CMS
+      onReady: (params) => {
+          setAuth(params.auth);
+          setValue(params.fieldValue ? params.fieldValue : "");
+          setTags(JSON.parse(params.fieldValue));
+          setFieldID(params.fieldID);
+          setFieldName(params.fieldName);
+          getSetTagOptions({ 
+            guid: params.auth.guid,
+            apiKey: params.auth.apiKey,
+            contentReferenceName: params.tagsContentReferenceName,
+            languageCode: params.auth.languageCode
+           });
+      }
+    })
+  }, []);
+
+  //when tags change, update our field value
+  useEffect(() => {
+    updateValue();
+  }, [tags])
+
+  const getSetTagOptions = async ({ guid, apiKey, languageCode, contentReferenceName}) => {
+    const api = agility.getApi({
+      guid,
+      apiKey,
+      isPreview: true
+    })
+
+    const tagOptionsResp = await api.getContentList({
+      referenceName: contentReferenceName,
+      languageCode
+    })
+
+    setTagOptions(tagOptionsResp.items);
+  }
+
+  const updateValue = () => {
+    const newVal = JSON.stringify(tags);
+    //update the react state
+    setValue(newVal);
+    //notify Agility CMS of the new value
+    updateFieldValue({ value: newVal, fieldName, fieldID });
+
+    console.log(newVal);
+  }
+
+  const updateTagValue = (index, tagValue) => {
+    let updatedTags = [...tags];
+    let updatedTag = {...updatedTags[index]};
+    updatedTag.tagValue = tagValue;
+    updatedTags[index] = updatedTag;
+
+    //update react state
+    setTags(updatedTags);
+    
+  }
+
+  const updateTagOption = (index, tagOptValue) => {
+    let updatedTags = [...tags];
+    let updatedTag = {...updatedTags[index]};
+    updatedTag.tagID = parseInt(tagOptValue);
+    updatedTags[index] = updatedTag;
+
+    //update react state
+    setTags(updatedTags);
+  }
+
+  const addTag = () => {
+    const newTag = {
+      tagID: -1,
+      tagValue: ""
+    }
+    //update react state
+    setTags(arr => [...arr, newTag]);
+  }
+
+  const removeTag = (index) => {
+    let updatedTags = [...tags];
+    updatedTags.splice(index, 1);
+
+    //update react state
+    setTags(updatedTags);
+  }
+  
+
+  return (
+    <div className="App" ref={containerRef}>
+      <div className="App_table__heading">
+        <div className="App_table__heading__col">Value</div>
+        <div className="App_table__heading__col">Tag</div>
+        <div className="App_table__heading__col">Actions</div>
+      </div>
+      {tags.map((tag, index) => (
+        <div className="App_table__row" key={tag.tagID}>
+          <div className="App_table__row__col">
+            <input style={{display: 'block'}} type="text" value={tag.tagValue} onChange={e => updateTagValue(index, e.target.value)} />
+          </div>
+          <div className="App_table__row__col">
+            <select value={tag.tagID} onChange={e => updateTagOption(index, e.currentTarget.value)}>
+              {tagOptions.map(opt => (
+                <option key={opt.contentID} value={opt.contentID}>
+                  {opt.fields.title}
+                </option>
+                )
+              )}
+            </select>
+          </div>
+          <div className="App_table__row__col">
+            <button onClick={e => removeTag(index)}>Remove</button>
+          </div>
+        </div>
+        )
+      )}
+      <button type="button" onClick={addTag}>Add</button>
+    </div>
+  );
+}
+
+export default App;
